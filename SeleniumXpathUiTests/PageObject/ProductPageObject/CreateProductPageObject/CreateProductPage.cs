c26ea70ef9dbc4ec99849398ba2ab0ede48a3ef9
doktorboba@outlook.com <doktorboba@outlook.com>
2023-01-19 20:11:22 +0300

    Описал страницу создания продукта селеккторами

--- SeleniumXpathUiTests/PageObject/ProductPageObject/CreateProductPageObject/CreateProductPage.cs
+++ SeleniumXpathUiTests/PageObject/ProductPageObject/CreateProductPageObject/CreateProductPage.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using SeleniumXpathUiTests.BrowserHelpers.Interface;
+using SeleniumXpathUiTests.PageObject.BasePageObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,48 @@ using System.Threading.Tasks;
 
 namespace SeleniumXpathUiTests.PageObject.ProductPageObject.CreateProductPageObject
 {
-    public class CreateProductPage
+    public class CreateProductPage : BasePage
     {
+        public CreateProductPage(IBrowser browser): base(browser)
+        {
 
+        }
+        // Заголовок поля создания продукта Name 
+        private IWebElement _productNameFieldTitle => browser.WebDriver.FindElement(
+            By.XPath("//div//label[@for='Product_Name']"));
+        // Поле ввода имени продукта Name
+        private IWebElement _productNameField => browser.WebDriver.FindElement(
+            By.XPath("//div//input[@id='Product_Name']]"));
+        // Заголовок поля Price
+        private IWebElement _productPriceFieldTitle => browser.WebDriver.FindElement(
+           By.XPath("//div//label[@for='Product_Price']"));
+        // Поле ввода цены продукта Price
+        private IWebElement _productPrieeFIeldT => browser.WebDriver.FindElement(
+            By.XPath("//div//input[@id='Product_Price']"));
+        // Заголовок поля Price
+        private IWebElement _productDescriptionFieldTitle => browser.WebDriver.FindElement(
+           By.XPath("//label[@for='Product_Description']"));
+        // Поле ввода цены продукта Price
+        private IWebElement _productDescriptionFIeld => browser.WebDriver.FindElement(
+            By.XPath("//div[@class='note-editable']"));
+        // Заголовок поля загрузки изображения Image
+        private IWebElement _productImageFIeld => browser.WebDriver.FindElement(
+        By.XPath("//div[contains(text(), 'Image')]"));
+
+        // Поле загрузки изображения Image
+        private IWebElement _productImageFIeldTitle => browser.WebDriver.FindElement(
+         By.XPath("//input[@type='file' and @id='uploadBox']"));
+        // Заколовок селектора выбора категории
+        private IWebElement _productCategoryFieldTitle => browser.WebDriver.FindElement(
+         By.XPath("//label[@for='Product_CategoryId']"));
+        // Поле селектора выбора категории товара 
+        private IWebElement _productCategorySelector=> browser.WebDriver.FindElement(
+         By.XPath("//select[@id='Product_CategoryId']"));
+        // Заголовок селектора выборака  ApptypeId
+        private IWebElement _producAppTypeIdSelectorTitle => browser.WebDriver.FindElement(
+         By.XPath("//label[@for='Product_AppTypeId']"));
+        // Поле селектора выбора AppTypeId товара 
+        private IWebElement _producAppTypeIdSelector => browser.WebDriver.FindElement(
+         By.XPath("//select[@id='Product_AppTypeId']"));
     }
 }
--- SeleniumXpathUiTests/PageObject/ProductPageObject/ProductPage.cs
+++ SeleniumXpathUiTests/PageObject/ProductPageObject/ProductPage.cs
@@ -17,5 +17,35 @@ namespace SeleniumXpathUiTests.PageObject.ProductPageObject
         //Кнопка создания продукта 
         private IWebElement _createNewProductButton => browser.WebDriver.FindElement(
             By.XPath("//a[@class='btn btn-primary fa-pull-right']"));
+        // Заглушка когда таблица с продуктами не создана 
+        private IWebElement _noProductExcistText => browser.WebDriver.FindElement(By.XPath("//p[text()=' No product exists.']"));
+        // Заголовок страницы 
+        private IWebElement _productPageTitle => browser.WebDriver.FindElement(
+            By.XPath("//h2[@class='text-primary']"));
+        // Таблица с созданными продуктами 
+        private IWebElement _productTable => browser.WebDriver.FindElement(
+            By.XPath("//table[@class='table table-bordered table-striped']"));
+        // Список со значениями Product Name 
+        private List<IWebElement> _productNamesList => browser.WebDriver.FindElements(
+           By.XPath("//td[@width='30%']")).ToList();
+        // Список со значениями Price 
+        private List<IWebElement> _priceList => browser.WebDriver.FindElements(
+          By.XPath("//td[@width='10%']")).ToList();
+        // Список со значениями Category 
+         private List<IWebElement> _categoryList => browser.WebDriver.FindElements(
+          By.XPath("//td[@test_id='Category Name']")).ToList();
+        // Список со значениями ApplicationType 
+        private List<IWebElement> _applicationType => browser.WebDriver.FindElements(
+         By.XPath("//td[@test_id='Category Name']")).ToList();
+        // Список заголовков таблицы продуктов 
+        private List<IWebElement> _prodcuctTableTitlesList => browser.WebDriver.FindElements(
+         By.XPath("//table[@class='table table-bordered table-striped']//th[text()]")).ToList();//a[@class='btn btn-primary mx-2']
+        // Список элементов кнопки редактирования в продукта в таблице 
+        private List<IWebElement> _updateProductButtonList => browser.WebDriver.FindElements(
+         By.XPath("//a[@class='btn btn-primary mx-2']")).ToList();
+        // Список кнопок удаления продуктов из таблицы 
+        private List<IWebElement> _deleteProductButtonList => browser.WebDriver.FindElements(
+        By.XPath("//a[@class='btn btn-primary mx-2']")).ToList();
     }
+
 }
